1bf55491b849e400ad85dd0db8945c2437ec067c
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-10-29 16:19:08 +0900

    ログイン・新規登録画面のビュー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  # NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
 
-  with_options presemce: true do
-    validate :name, format:{with: NAME_REGEX, message: 'は全角で入力してください'}
-  end
+  # with_options presemce: true do
+  #   validates :name, format: {with: NAME_REGEX, message: 'は全角で入力してください'}
+  # end
 end
