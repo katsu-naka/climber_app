2ea440e770b9375719572355db61cfb86b96293a
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-10-29 19:02:16 +0900

    tasksコントローラー編集
    new crearteアクション追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,24 @@
 class TasksController < ApplicationController
   def index
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :text, :date ,:done).merge(user_id: current_user.id)
+  end
+
 end
