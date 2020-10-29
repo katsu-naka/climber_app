0b10dda0ecee68c787f352f413dfc4df3c3160bd
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-10-29 16:11:20 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+
+  with_options presemce: true do
+    validate :name, format:{with: NAME_REGEX, message: 'は全角で入力してください'}
+  end
 end
