4aff88c67a1abf279d41bb779c7e3512cd2b9574
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-11-02 13:49:00 +0900

    inquiryモデル・テーブルの作成
    問い合わせ内容を保存する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_021914) do
+ActiveRecord::Schema.define(version: 2020_11_02_044814) do
+
+  create_table "inquiries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "project_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "project_id"
