8a88461181de8983e04f4130165f4c8a62126b3e
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-11-02 11:26:27 +0900

    usersコントローラー作成、ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   root to: 'tasks#index'
   resources :tasks do
     post 'done_update', to: 'tasks#done_update' 
