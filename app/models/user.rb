744973c45c6a8dfb59db0a177cd598bde2b44e37
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-11-02 11:23:31 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   has_many :tasks
   has_many :projects , through: :project_users
   has_many :project_users
+  has_many :sns_credentials
 
 
   NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
