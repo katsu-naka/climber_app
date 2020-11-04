01e6d8dd57efbec26d7b834f48c84d398e8e0b25
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-11-04 16:23:01 +0900

    production.rbの編集

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "climber_app_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                  587,
+  domain:               'gmail.com',
+  user_name:            ENV['MAIL'],
+  password:             ENV['GOOGLE_APP_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto:  true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
