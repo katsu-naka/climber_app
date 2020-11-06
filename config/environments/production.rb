b71db826679f48adf09713a0c0ed076d365d7233
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-11-06 16:22:21 +0900

    本番環境でのコメント表示不具合の修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,6 +101,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  ActionCable.server.config.disable_request_forgery_protection = true
+  config.action_cable.url = 'ws://54.178.5.191/cable'
+  config.action_cable.allowed_request_origins = [ 'http://54.178.5.191' ]  
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
