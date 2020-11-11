29e4fa4f24d35ec2ecc1ddb349f671dda4f84b71
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-11-11 11:40:44 +0900

    ログイン機能の結合テストコード実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -57,3 +57,49 @@ RSpec.describe "ユーザー新規登録", type: :system do
     end
   end
 end
+
+RSpec.describe "ユーザーログイン", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
+  context 'ログインが成功する' do
+    it '正しい情報を入力すればログインができてトップページに遷移する' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログイン画面へ遷移するボタンがあることを確認する
+      expect(page).to have_content("ログイン")
+      # ログインページへ移動する
+      visit new_user_session_path
+      # ユーザー情報を入力する
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      # ログインボタンをクリックする
+      find('input[name="commit"]').click
+      # トップページに遷移する
+      expect(current_path).to eq root_path
+      # プルダウンメニューをクリックするととログアウトボタンが表示されることを確認する
+      find('.btn').click
+      expect(page).to have_content("ログアウト")
+      # 新規登録画面へ遷移するボタンや、ログイン画面に遷移するボタンがないか確認する
+      expect(page).to have_no_link('新規登録')
+      expect(page).to have_no_link('ログイン')
+    end
+  end
+  context 'ログインが失敗する' do
+    it '誤った情報ではログインできずログイン画面に留まる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにログイン画面へ遷移するボタンがあることを確認する
+      expect(page).to have_content("ログインしてプロジェクトに参加する")
+      # ログインページへ移動する
+      visit new_user_session_path
+      # ユーザー情報を入力する
+      fill_in 'メールアドレス', with: ""
+      fill_in 'パスワード', with: ""
+      # ログインボタンをクリックする
+      find('input[name="commit"]').click
+      #  ログインページに留まることを確認する
+      expect(current_path).to eq new_user_session_path
+    end
+  end
+end
