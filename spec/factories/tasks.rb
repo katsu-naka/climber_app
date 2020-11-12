b7777fc130a1813f7673c824fdbf9b7c60629261
katsu-naka <71310171+katsu-naka@users.noreply.github.com>
2020-11-12 09:47:34 +0900

    タスク編集機能の結合テストコード実装

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :task do
-    date   {Faker::Date.in_date_period}
+    date   {Date.today}
     title  {Faker::Types.rb_string}
     text   {Faker::Types.rb_string }
     done   { 1 }
